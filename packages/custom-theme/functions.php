6d8267b90d5b43d1aa09fa7bd5461feed6e12d24
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-04-11 17:24:01 +0700

    Add `.editorconfig` and update `.gitignore`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- packages/custom-theme/functions.php
+++ packages/custom-theme/functions.php
@@ -3,9 +3,9 @@
 add_action( 'wp_enqueue_scripts', 'custom_theme_scripts' );
 
 function custom_theme_scripts() {
-    $theme = wp_get_theme( get_stylesheet() );
+	$theme = wp_get_theme( get_stylesheet() );
 
-    wp_register_script( $theme->stylesheet, get_stylesheet_directory_uri() . '/custom.js', [], $theme->version, ['defer'] );
+	wp_register_script( $theme->stylesheet, get_stylesheet_directory_uri() . '/custom.js', [], $theme->version, ['defer'] );
 
-    wp_enqueue_script( $theme->stylesheet );
+	wp_enqueue_script( $theme->stylesheet );
 }
