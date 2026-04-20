2cdac604a292324806a14a5a6969ed8461a0e143
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-04-20 20:51:49 +0700

    Add sample plugin tests
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ tests/units/CF7_Entry_Manager/PluginTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace UnitTests\CF7_Entry_Manager;
+
+use UnitTests\BaseTestCase;
+use Brain\Monkey\Actions;
+use Brain\Monkey\Filters;
+use Brain\Monkey\Functions;
+
+/**
+ * Class PluginTest
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
+class PluginTest extends BaseTestCase {
+	/**
+	 * Test that constants are defined and hooks are registered on load.
+	 */
+	public function test_plugin_initialization() {
+		// Mock WP functions used in the main file
+		Functions\when( 'esc_html__' )->returnArg( 1 );
+		Functions\when( 'esc_html_e' )->echoArg( 1 );
+		Functions\when( '__' )->returnArg( 1 );
+		Functions\when( '_x' )->returnArg( 1 );
+		Functions\when( 'register_activation_hook' )->justReturn();
+		Functions\when( 'register_deactivation_hook' )->justReturn();
+		Functions\when( 'plugin_dir_url' )->justReturn( 'https://example.com/wp-content/plugins/cf7-entry-manager/' );
+		Functions\when( 'register_post_type' )->justReturn();
+
+		// Set WP version global
+		$GLOBALS['wp_version'] = '6.9';
+
+		// Expect hooks to be added
+		Actions\expectAdded( 'admin_notices' )->never();
+		Actions\expectAdded( 'admin_enqueue_scripts' )->once();
+		Actions\expectAdded( 'wpcf7_init' )->once();
+		Actions\expectAdded( 'init' )
+			->once()
+			->whenHappen(
+				function ( $callback ) {
+					Filters\expectAdded( 'user_contactmethods' )->once();
+					$callback();
+				}
+			);
+
+		// Load the plugin file
+		require dirname( ABSPATH, 3 ) . '/packages/cf7-entry-manager/cf7-entry-manager.php';
+
+		// Verify constants
+		$this->assertTrue( defined( 'CF7EM_VERSION' ) );
+		$this->assertEquals( '0.1.0', CF7EM_VERSION );
+		$this->assertTrue( defined( 'CF7EM__MINIMUM_WP_VERSION' ) );
+		$this->assertTrue( defined( 'CF7EM__MINIMUM_PHP_VERSION' ) );
+
+		$this->addToAssertionCount( 5 );
+	}
+}
---
+++ tests/units/CF7_Entry_Manager/TestCase.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace UnitTests\CF7_Entry_Manager;
+
+use UnitTests\BaseTestCase;
+
+/**
+ * Base Test Case for all unit tests.
+ */
+abstract class TestCase extends BaseTestCase {
+	/**
+	 * Setup before any test in this class runs.
+	 */
+	public static function setUpBeforeClass(): void {
+		parent::setUpBeforeClass();
+
+		if ( ! class_exists( 'WPCF7_HTMLFormatter' ) ) {
+			eval(
+				'class WPCF7_HTMLFormatter {
+				public const placeholder_block = "pb";
+				public const placeholder_inline = "pi";
+				public const void_elements = ["br", "hr", "input", "img"];
+				public const p_parent_elements = ["div"];
+				public const p_nonparent_elements = ["p"];
+				public const p_child_elements = ["span"];
+				public const br_parent_elements = ["div"];
+				public function __construct($opt) {}
+				public function append_start_tag($t, $a) {}
+				public function append_preformatted($c) {}
+				public function end_tag($t) {}
+				public function append_comment($c) {}
+				public function print() { echo "rendered"; }
+			}'
+			);
+		}
+	}
+}
